de3a8f558f7ca8799045e6cd0eb77ce349ea38e6
amiraliniyazzmand <amiraliniyazmand6@gmail.com>
2024-08-31 16:24:36 +0330

    first commit

---
+++ jalaseh.py/tamrin1.py
@@ -0,0 +1,9 @@
+try:
+    age1 = int(input("sen khod ra vard knid"))
+    if age1 >=18:
+        print("nmitvani ray bdi")
+    else:
+        print("nmitvani ray bdi")
+except ValueError:
+    print("add shih vard knid")
+print("payan")
