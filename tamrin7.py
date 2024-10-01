3515de50cbdd5f8c82799ea1c4e3b0bcab72f85a
amiraliniyazmand27@gmail.com <amiraliniyazmand27@gmail.com>
2024-10-01 18:48:52 +0330

    tamrin7

---
+++ tamrin7.py
@@ -0,0 +1,16 @@
+list_adad = []
+tavan= lambda adad:adad ** 2
+while True :
+    adad = input("adad hayy kh makhahi bh tvan 2 briseh bnvis :")
+    if  adad == "tavan":
+        break
+    else:
+        adad= int(adad)
+
+        list_adad.append(adad)
+
+        tavan(adad)
+
+        payani = map( tavan , list_adad)
+    payani = list(payani)
+    print(f"Javab Tavan 2 : {payani}")
