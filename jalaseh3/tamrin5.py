c2f40d4c752ad6a510fb0d7efade1012e6a66c1b
amiraliniyazmand27@gmail.com <amiraliniyazmand27@gmail.com>
2024-10-01 18:04:40 +0330

    tamrin5

---
+++ jalaseh3/tamrin5.py
@@ -0,0 +1,19 @@
+def fibonachi(adad):
+    if adad == 1 or adad == 2:
+        return 1
+    else:
+        return fibonachi(adad - 1) + fibonachi(adad -2)
+if __name__ == "__main__":
+    while True:
+        try:
+            inputnumber = int(input("tedad list khod ra bnvisid: "))
+            if inputnumber < 1:
+                print("lotfan y; adad bala 1 bnvisid.")
+                continue
+            fibonachilist = []
+            for i in range(1, inputnumber + 1):
+                fibonachilist.append(fibonachi(i))
+            print(" , ".join(map(str, fibonachilist)))
+            break
+        except ValueError:
+            print("lotfan adad sahih bnvisid.")
